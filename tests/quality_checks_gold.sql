0725943499dcc474b30bf8e78c3c4f3ac6967a8f
bkinkead577 <76488255+bkinkead577@users.noreply.github.com>
2025-05-11 08:40:41 -0400

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,3 +1,23 @@
+/*
+============================================================
+Quality Checks 
+=========================================================
+
+Script Purpose:
+   This script performs quality checks to validate the integrity, consistency,
+  and accuracy of the Gold layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity beten fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+
+Usage Notes:
+   - Run these checks after data loading Silver latyer.
+   - Investigate and resolve and discrepencies found during the checks.
+*/
+
+
+
 --=============================================
   -- Check Foreign Key Integrity
 --==============================================
